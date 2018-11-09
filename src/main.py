16cac66856240547eca8af964d52323c4dba9a46
silverjoda <heizen.sh@gmail.com>
2018-11-09 16:15:46 +0100

    m

--- src/main.py
+++ src/main.py
@@ -3,59 +3,36 @@ from networks import AVA
 from audio_DS import AudioSet
 import numpy as np
 import cv2
-
+import torch as T
 
 def main():
 
     LOAD = False
 
     imres = 128
-    FPS = 30
-    samplerate = 16384
+    FPS = 60
+    samplerate = 44100
     n_iters = 20000
-    batchsize = 16
-
-    # Make network
-    network = AVA("testnet", samplerate, FPS)
+    batchsize = 32
 
     # Make dataset
     video_reader = VidSet(imres)
+    audio_reader = AudioSet()
 
-    if LOAD:
-        # Load weights
-        network.restore_weights()
-    else:
-        # Train
-        for i in range(n_iters):
-            batch = video_reader.get_rnd_sample(batchsize)
-            mse = network.train(batch)
+    # Make network
+    network = AVA("testnet", samplerate, FPS)
+    #network = T.load("trained_networks/ava.pt")
 
-            if i % 10 == 0:
-                print("Iteration {}/{}, mse: {}".\
-                    format(i, n_iters, mse))
-
-        # Save weights
-        network.save_weights()
+    # Train ==============
+    network.train()
 
     # Test ===============
-    test_sample = video_reader.get_cons_sample(300)
+    test_sample = audio_reader.get_cons_sample(3)
     reconstruction = network.reconstruct(test_sample)
     assert test_sample.shape == reconstruction.shape, print("Reconstruction does not match initial video")
-    combined_video = np.concatenate((test_sample, reconstruction), axis=2)
 
-    writer = cv2.VideoWriter('video_results/test1.avi', cv2.VideoWriter_fourcc(*'PIM1'), 25, (2 * imres, imres), False)
-    for c in combined_video:
-        frame = c.astype(np.uint8)
-        #x = np.random.randint(255, size=(imres, 2 * imres)).astype('uint8')
-        writer.write(frame)
-
-    audio_encoding = network.encode(test_sample)
-    audio_encoding = np.reshape(audio_encoding, [-1])
-
-    audio_writer = Audiowriter()
-    audio_writer.writetofile(audio_encoding,
-                             "firsttest.wav",
-                             samplerate)
+    vid_encoding = network.encode(test_sample)
+    video_reader.write_video(vid_encoding, "test_encoding")
 
 
 
